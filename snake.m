d52a71be519a02d2e748e78aebd771b2d5db0237
Подозерова Полина <59765800+polinapodozerova@users.noreply.github.com>
2023-06-02 21:31:10 +0300

    Update snake.m
    исправила ошибку с убегающим вперед остальных первым кружочком

--- snake.m
+++ snake.m
@@ -14,35 +14,33 @@ x = zeros(1, n);
 y = zeros(1, n);
 h = zeros(1, n);
 speed = zeros(1, n);
-angle = zeros(1, n);
+
 phi = 0;
 for j = 1:n
   x(j) = xlim(1) + r + pi/12 * j;
   y(j) = sin(x(j));
   h(j) = plot(x(j) + r*cos(t), y(j) + r*sin(t), 'Color', rand(1, n), 'Linewidth', 2);
-  speed(j) = 0.1;
-  angle(j) = 1;
+  speed(j) = 0.2;
+
 end
 axis([xlim(1) xlim(2) ylim(1) ylim(2)]);
 
 %% Move circles
 while true
-  
-  for i = 1:n
-    % Move circle 1
-    x(i) = x(i) + speed(i);
-    y(i) = sin(x(i));
+  for i = 1:n-1
+    x(i) = x(i + 1);
+    y(i) = y(i+1);
     set(h(i), 'XData', x(i)*cos(phi) - y(i) * sin(phi) + r*cos(t), 'YData', x(i)*sin(phi) + y(i)*cos(phi) + r*sin(t));
     
-    % Check for bounce off left or right wall
     if x(i) < (xlim(1) + r) || x(i) > (xlim(2) - r)
         speed(i) = -speed(i);
-        % phi = (x(i)*sin(phi) + y(i)*cos(phi))';
-    end
-##    if y(i) < (ylim(1) + r) || y(i) > (ylim(2) - r)
-##        speed(i) = -speed(i);
-##        % phi = -(x(i)*cos(phi) - y(i) * sin(phi))';
-##    end
-  end
+    endif
+  endfor
+  x(n) = x(n) + speed(n);
+  y(n) = sin(x(n));
+  set(h(n), 'XData', x(n)*cos(phi) - y(n) * sin(phi) + r*cos(t), 'YData', x(n)*sin(phi) + y(n)*cos(phi) + r*sin(t));
+  if x(n) < (xlim(1) + r) || x(n) > (xlim(2) - r)
+      speed(n) = -speed(n);
+  endif
   pause(0.05);
 end
